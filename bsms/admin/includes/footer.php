89e0b97bf0481feaec348776e38e2560e2d028be
AHMAD SOLEHIN BIN ASMADI <solehinahmad954@gmail.com>
2024-07-02 21:24:46 +0800

    UPDATE ADMIN FOOTER

--- bsms/admin/includes/footer.php
+++ bsms/admin/includes/footer.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a href="http://code4berry.com">Code4berry</a>.</strong>
+  <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a href="https://www.instagram.com/mankraftbarbershop/">Mankraft Barbershop</a>.</strong>
   All rights reserved.
 </footer>
