cc276ca4068e51badab0c42e450985c9b7f1e47d
AHMAD SOLEHIN BIN ASMADI <solehinahmad954@gmail.com>
2024-07-03 15:41:40 +0800

    DONE ALL

--- bsms/admin/blockedusers.php
+++ bsms/admin/blockedusers.php
@@ -56,7 +56,6 @@ if(isset($_GET['blockid']))
 </table>
 </div>
 <!-- /.card-body -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script type="text/javascript">
     $(document).ready(function(){
       $('.delete_user').on('click', function(){
