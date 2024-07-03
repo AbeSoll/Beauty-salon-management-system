5f21c3663f0b2facd7e9a183731a5ec633ff6c0b
AHMAD SOLEHIN BIN ASMADI <solehinahmad954@gmail.com>
2024-07-03 09:14:25 +0800

    delete button not all/new/accept/reject appointment file

--- bsms/admin/accepted_appointment.php
+++ bsms/admin/accepted_appointment.php
@@ -99,7 +99,9 @@ if (strlen($_SESSION['sid']==0)) {
                         <td><?php  echo $row['AptDate'];?></td> 
                         <td><?php  echo $row['AptTime'];?></td> 
                         <td>
-                          <a href="#" class="btn btn-sm btn-primary edit_data" id="<?php echo  $row['ID']; ?>" title="click for edit">View</a></td> 
+                          <a href="#" class="btn btn-sm btn-primary edit_data" id="<?php echo  $row['ID']; ?>" title="click for edit">View</a>
+                          <a href="#" class="btn btn-sm btn-danger delete_data" id="<?php echo $row['ID']; ?>" title="click for delete">Delete</a>
+                        </td> 
                       </tr>   
                       <?php 
                       $cnt=$cnt+1;
@@ -147,5 +149,23 @@ if (strlen($_SESSION['sid']==0)) {
     });
   });
 </script>
+<script type="text/javascript">
+    $(document).ready(function(){
+      $(document).on('click', '.delete_data', function(){
+        var delete_id = $(this).attr('id');
+        if(confirm("Are you sure you want to delete this appointment?")){
+            $.ajax({
+                url: "delete_appointment.php",
+                type: "POST",
+                data: {delete_id: delete_id},
+                success: function(response){
+                    alert(response);
+                    location.reload(); // Reload the page to reflect changes
+                }
+            });
+        }
+      });
+    });
+  </script>
 </body>
 </html>
