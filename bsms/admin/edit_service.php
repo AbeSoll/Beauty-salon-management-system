e83d3135cc1f6b712f186afb9970e018749aaf25
AHMAD SOLEHIN BIN ASMADI <solehinahmad954@gmail.com>
2024-07-05 01:58:24 +0800

    EDIT SERVICE DOUBLE CONFIRMATION

--- bsms/admin/edit_service.php
+++ bsms/admin/edit_service.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
@@ -9,27 +8,24 @@ if(isset($_POST['submit']))
   $cost=$_POST['cost'];
 
   $eid=$_SESSION['edid'];
-  $query=mysqli_query($con, "update  tblservices set ServiceName='$sername',Cost='$cost' where ID='$eid' ");
+  $query=mysqli_query($con, "update tblservices set ServiceName='$sername', Cost='$cost' where ID='$eid' ");
   if ($query) {
-    //$msg="Service has been Updated.";
     echo '<script>alert("Service has been updated")</script>';
   }
   else
   {
     echo '<script>alert("Something Went Wrong. Please try again")</script>';
-    //$msg="Something Went Wrong. Please try again";
   }
 }
 ?>
 <h4 class="card-title">Update Service Form </h4>
-<form role="form" method="post">
+<form role="form" method="post" onsubmit="return confirmUpdate()">
   <p style="font-size:16px; color:red" align="center"> <?php if($msg){
     echo $msg;
   }  ?> </p>
   <?php
   $eid=$_POST['edit_id'];
-  $ret=mysqli_query($con,"select * from  tblservices where ID='$eid'");
-  $cnt=1;
+  $ret=mysqli_query($con,"select * from tblservices where ID='$eid'");
   while ($row=mysqli_fetch_array($ret))
   {
     $_SESSION['edid']=$row['ID'];
@@ -37,11 +33,11 @@ if(isset($_POST['submit']))
     <div class="card-body">
       <div class="form-group">
         <label for="exampleInputEmail1">Service Name</label>
-        <input type="text" class="form-control" id="sername" name="sername" placeholder="Service Name" value="<?php  echo $row['ServiceName'];?>" required="true">
+        <input type="text" class="form-control" id="sername" name="sername" placeholder="Service Name" value="<?php echo $row['ServiceName'];?>" required="true">
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Cost</label>
-        <input type="text" id="cost" name="cost" class="form-control" placeholder="Cost" value="<?php  echo $row['Cost'];?>" required="true">
+        <input type="text" id="cost" name="cost" class="form-control" placeholder="Cost" value="<?php echo $row['Cost'];?>" required="true">
       </div>
       <?php 
     } ?>
@@ -54,3 +50,9 @@ if(isset($_POST['submit']))
   </div>
   <!-- /.card-body -->
 </form>
+
+<script type="text/javascript">
+  function confirmUpdate() {
+    return confirm('Are you sure you want to update this service?');
+  }
+</script>
